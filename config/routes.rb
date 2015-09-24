f17137877fe388fee14f530c3f7f9e4308593098
jvrsgsty <jvrsgsty@users.noreply.github.com>
2015-09-24 13:03:40 -0500

    Set the route constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-
+  # Api definition
+  # Rails will automatically map that namespace to a directory matching the 
+  # name under the controllers folder
+  namespace :api, defaults: {format: :json}, 
+  								constraints: { subdomain: 'api' }, path: '/' do
+    # We are going to list our resources here
+  end
 end
 
